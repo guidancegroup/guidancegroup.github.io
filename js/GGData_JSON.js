2c080c8961a2be8e56c0b44f3a5ecdc0b96de59b
guidancegroup <37658320+guidancegroup@users.noreply.github.com>
2018-04-02 22:36:24 +0530

    download list removed

--- js/GGData_JSON.js
+++ js/GGData_JSON.js
@@ -23,36 +23,6 @@ $rootScope.courseData={
     ]
 }
 	
-//download material entry
-$rootScope.downloadsData={
-	"downloads":[
-		{
-			"index":"1",
-			"examination":"MPSC",
-			"syllabus":"#",
-			"booksList":"#",
-			"previousQuestions":"#",
-			"notes":"#"
-		},
-		{
-			"index":"2",
-			"examination":"UPSC",
-			"syllabus":"#",
-			"booksList":"#",
-			"previousQuestions":"#",
-			"notes":"#"
-		},
-		{
-			"index":"3",
-			"examination":"MLEP",
-			"syllabus":"#",
-			"booksList":"#",
-			"previousQuestions":"#",
-			"notes":"#"
-		}
-	]
-}
-	
 //mentors entry
 $rootScope.mentorData={
   "mentors":[
