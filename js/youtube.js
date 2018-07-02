1e547c875db2df4130c4084f6d5c56bfc09c38b1
guidancegroup <37658320+guidancegroup@users.noreply.github.com>
2018-07-03 00:19:49 +0530

    Update youtube.js

--- js/youtube.js
+++ js/youtube.js
@@ -2,102 +2,18 @@ var youtube={
 		"videos":[
 			{
 				"player":"player1",
-				"id":"LezW1WYGc9A"
-			},
-			{
-				"player":"player2",
-				"id":"fOpoehTmlqQ"
-			},
-			{
-				"player":"player3",
-				"id":"iv4uM8qulTo"
-			},
-			{
-				"player":"player4",
-				"id":"iyJdER8sEhA"
-			},
-			{
-				"player":"player5",
-				"id":"KqL-TdJie28"
-			},
-			{
-				"player":"player6",
-				"id":"kcWe4NV6FeI"
-			},
-			{
-				"player":"player7",
-				"id":"LA3I_-WyhpU"
-			},
-			{
-				"player":"player8",
-				"id":"DWbQEQhduWU"
-			},
-			{
-				"player":"player9",
-				"id":"MxtnViH_z3A"
-			},
-			{
-				"player":"player10",
-				"id":"rLNnSdBPGVM"
-			},
-			{
-				"player":"player11",
-				"id":"MjuwX1B3ego"
-			},
-			{
-				"player":"player12",
-				"id":"Ix4Qw4saADk"
-			},
-			{
-				"player":"player13",
-				"id":"Eb2S1walZOs"
-			},
-			{
-				"player":"player14",
-				"id":"tWqZVGGQ3og"
-			},
-			{
-				"player":"player15",
-				"id":"vigbqZ7Bh_Y"
-			},
-			{
-				"player":"player16",
-				"id":"dD_87EKuRx8"
-			},
-			{
-				"player":"player17",
-				"id":"3-MbsdSYMBk"
-			},
-			{
-				"player":"player18",
-				"id":"6TH0x79IyqQ"
-			},
-			{
-				"player":"player19",
-				"id":"kULX2Lwo51U"
-			},
-			{
-				"player":"player20",
-				"id":"9VVjW4am9yg"
-			},
-			{
-				"player":"player21",
-				"id":"Qut7Yr7-WlM"
-			},
-			{
-				"player":"player22",
 				"id":"FskcFc1zpJE"
 			},
 			{
-				"player":"player23",
+				"player":"player2",
 				"id":"9j4QJ8YK8n8"
 			},
 			{
-				"player":"player24",
+				"player":"player3",
 				"id":"oMG_J0RJ_j8"
 			},
 			{
-				"player":"player25",
+				"player":"player4",
 				"id":"9j4QJ8YK8n"
 			}
 			
