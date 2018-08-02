90e51a144fed4cd7335fc922c76f481705bf5b97
guidancegroup <37658320+guidancegroup@users.noreply.github.com>
2018-08-02 11:54:35 +0530

    Update onlinetestplatform.js

--- OnlineTestPlatform/js/onlinetestplatform.js
+++ OnlineTestPlatform/js/onlinetestplatform.js
@@ -1,5 +1,18 @@
 var platform= angular.module("OnlineTestPlatform",[]);
 
+platform.factory('storageService', ['$rootScope', function($rootScope) {
+
+    return {
+        get: function(key) {
+            return localStorage.getItem(key);
+        },
+        set: function(key, data) {
+            localStorage.setItem(key, data);
+        }
+    };
+}]);
+
+
 $(function () {
 	  $('[data-toggle="tooltip"]').tooltip()
 	})
